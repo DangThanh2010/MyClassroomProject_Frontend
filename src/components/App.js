15a3b762821ff122f145fda1e8e502cc9dbc4018
HuyThien <57531802+HuyThien@users.noreply.github.com>
2021-11-21 23:16:41 +0700

    Add feature: upload avatar, change avatar

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import { BrowserRouter as Routes, Switch, Route, Link  } from "react-router-dom";
 import Home from '../components/home';
 import LoginPage from '../components/authentication/LoginPage';
-
+import Detail from '../components/DetailClass/DetailClass'
 function App() {
   return (
     <div className="App">
@@ -10,6 +10,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="login" element={<LoginPage />} />
       </Routes> */}
+      <Detail></Detail>
     </div>
   );
 }
