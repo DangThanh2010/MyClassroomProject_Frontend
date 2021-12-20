15ff14904a2dd57996f3d544d4828a85fa94744c
thientam075 <72021036+thientam075@users.noreply.github.com>
2021-12-20 18:00:25 +0700

    Fix Table

--- src/components/Grade/detailGrade.js
+++ src/components/Grade/detailGrade.js
@@ -1,41 +1,99 @@
-import {
-  TableRow,
-  TableCell,
-  OutlinedInput,
-  Input
-} from "@mui/material";
+import { TableRow, TableCell, Input, Button } from "@mui/material";
 
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
+export default function DetailGrade({
+  rows,
+  page,
+  rowsPerPage,
+  columns,
+  handleSend,
+}) {
+  const [idEdit, setIdEdit] = useState(null);
+  const [valueEdit, setValueEdit] = useState(null);
+  const handleChange = (event, id) => {
+    if (idEdit) {
+      setIdEdit(id);
+    }
+    if (idEdit !== id) {
+      setIdEdit(id);
+      setValueEdit(event.target.value);
+    } else {
+      setValueEdit(event.target.value);
+    }
+    console.log(idEdit);
+    console.log(valueEdit);
+  };
+  const onSave = () => {
+    console.log(idEdit + valueEdit);
+    if (valueEdit && idEdit) {
+      handleSend(idEdit, valueEdit);
+    } else {
+      alert("Something went wrong, Please reload the page");
+    }
+    setIdEdit(null);
+    setValueEdit(null);
+  };
 
-export default function DetailGrade({ rows, page, rowsPerPage, columns }) {
+  const flexContainer = {
+    display: "flex",
+    flexDirection: "row",
+    padding: 0,
+    justifyContent: "flex-end",
+  };
+  
   return (
     <>
-      {rows
-        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-        .map((row) => {
-          const name = row.fullName
-            ? `${row.fullName}(${row.studentId})`
-            : row.studentId;
-          return (
-            <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
-              <TableCell>{name}</TableCell>
-              {columns.map((column, index) => {
-                let value;
-                if (column.id === row.AssignmentId) {
-                  value = row.point;
-                }
-                return (
-                  <>
-                    <TableCell key={column.id} align={"right"} sx = {{border: 1}} minWidth={2}>
-                      {value} / 100
-                    </TableCell>
-                  </>
-                );
-              })}
-            </TableRow>
-          );
-        })}
+      {rows.map((row, index) => {
+        const name = row.fullName
+          ? `${row.fullName}(${row.studentId})`
+          : row.studentId;
+        return (
+          <TableRow
+            hover
+            role="checkbox"
+            tabIndex={-1}
+            key={row.id + index + row.studentId}
+          >
+            <TableCell>{name}</TableCell>
+            {columns.map((column, index) => {
+              let value;
+              if (column.id === row.AssignmentId) {
+                value = row.point;
+              }
+              return (
+                <TableCell
+                  key={column.id + row.studentId + index}
+                  align={"right"}
+                  sx={{ border: 1 }}
+                >
+                  <div style={flexContainer}>
+                    <Input
+                      defaultValue={value}
+                      className="input-grade"
+                      key="input"
+                      onChange={(event) => handleChange(event, row.id)}
+                    />
+                    <div
+                      key="maxGrade"
+                      style={{
+                        textAlign: "center",
+                        paddingTop: "2.5%",
+                        fontSize: 16,
+                      }}
+                    >
+                      / 100
+                    </div>
+                    <Button key="btn-Save" onClick={onSave}>
+                      Save
+                    </Button>
+                  </div>
+                </TableCell>
+              );
+            })}
+          </TableRow>
+        );
+      })}
     </>
   );
 }
--- src/components/Grade/listGrade.js
+++ src/components/Grade/listGrade.js
@@ -13,72 +13,136 @@ import DetailGrade from "./detailGrade";
 import ExportStudent from "./exportStudent";
 import ExportGrade from "./exportGrade";
 
-
-
 export default function ListGrade({}) {
   const idClass = 1;
   const [isLoaded, setIsLoaded] = useState(false);
   const [error, setError] = useState(false);
-  const [page, setPage] = useState(0);
   const [columns, setColumns] = useState([]);
   const [rows, setRows] = useState([]);
-  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [data, setData] = useState(
+    Array.from({ length: rows.length }, () =>
+      Array.from({ length: columns.length })
+    )
+  );
 
   useEffect(() => {
     const token = getToken();
     fetchColumnsData(token);
     fetchRowsData(token);
-    
+    setData(feactTableData(rows, columns));
   }, [isLoaded]);
 
   const getToken = () => {
     let token = "";
-    if(localStorage.getItem("token")){
+    if (localStorage.getItem("token")) {
       token = localStorage.getItem("token").slice(1);
       token = token.slice(0, -1);
     }
     return token;
-  }
+  };
+
+  const handleSend = (id, point) => {
+    const token = getToken();
+    fetchGradeData(token, id, point);
+  };
+
+  const fetchGradeData = (token, id, point) => {
+    fetch(process.env.REACT_APP_API + "/grade", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+      body: JSON.stringify({
+        id: id,
+        point: point,
+      }),
+    }).then((res) => {
+      if (!res.ok) {
+        setError(true);
+        console.log(res);
+      } else {
+        res.json().then((result) => {
+          if (result) {
+            setIsLoaded(true);
+          }
+        });
+      }
+    });
+  };
+
   const fetchColumnsData = (token) => {
     fetch(process.env.REACT_APP_API + "/assignment/" + idClass, {
-      headers: {'Content-Type':'application/json',
-                Authorization: 'Bearer ' + token},
-      })
-      .then(res => {
-        if (!res.ok) {
-          setError(true);
-        } else {
-          res.json().then((result) => {
-            if (result) {
-              setIsLoaded(true);
-              setColumns(result);
-            }
-          });
-        }
-      })
-  }
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+    }).then((res) => {
+      if (!res.ok) {
+        setError(true);
+      } else {
+        res.json().then((result) => {
+          if (result) {
+            setColumns(result);
+            setIsLoaded(true);
+            
+          }
+        });
+      }
+    });
+  };
 
   const fetchRowsData = (token) => {
     fetch(process.env.REACT_APP_API + "/grade/" + idClass, {
-      headers: {'Content-Type':'application/json',
-                Authorization: 'Bearer ' + token},
-      })
-      .then(res => {
-        if (!res.ok) {
-          setError(true);
-        } else {
-          res.json().then((result) => {
-            if (result) {
-              setIsLoaded(true);
-              setRows(result.data);
-            }
-          });
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+    }).then((res) => {
+      if (!res.ok) {
+        setError(true);
+      } else {
+        res.json().then((result) => {
+          if (result) {
+            setRows(result.data[0]);
+            setIsLoaded(true);
+            
+          }
+        });
+      }
+    });
+  };
+
+  const feactTableData = (rows, columns) => {
+    if(rows || columns) {
+      return null;
+    }
+    let dataTable = [];
+    let objects = [];
+    for (let j = 0; j < rows.length; j++) {
+      objects["studentId"] = rows[j].studentId;
+      objects["fullName"] = rows[j].fullName;
+      for (let i = 0; i < columns.length; i++) {
+        objects[columns[i].name] = "";
+        if (rows[j].name === columns[i].name) {
+          objects[columns[i].name] = rows[j].point;
         }
-      })
-  }
+      }
+      console.log(objects);
+      if (j + 1 < rows.length && objects["studentId"] !== rows[j].studentId) {
+        dataTable.push(objects);
+        objects = [];
+      }
+      if (j + 1 === rows.length) {
+        dataTable.push(objects);
+      }
+    }
+    console.log(objects);
+    return dataTable;
+  };
   return (
     <div>
-     <ExportStudent />
+      <ExportStudent />
       <ExportGrade />
 
       <Paper sx={{ width: "100%", overflow: "hidden" }}>
@@ -90,9 +154,8 @@ export default function ListGrade({}) {
             <TableBody>
               <DetailGrade
                 rows={rows}
-                page={page}
-                rowsPerPage={rowsPerPage}
                 columns={columns}
+                handleSend={handleSend}
               ></DetailGrade>
             </TableBody>
           </Table>
